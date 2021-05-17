0a869f31feaa86dec0e4dcc9e97b9289b71d5e73
Diego Borigen <diego@iddigital.com.au>
2021-05-17 20:51:40 +1000

    Add transitions

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import {
     Switch,
     Route
 } from 'react-router-dom';
-import './App.css';
 //styles
+import './App.scss';
 import './css/global-styles.scss';
 //components
 import Banner from './components/banner';
@@ -15,43 +15,61 @@ import OurServices from './components/ourServices';
 import Performance from './components/performance';
 import Contact from './components/contact';
 import { SERVICES_DATA } from './components/servicesPage/ServicesItem/constants';
-import ServicesItem from "./components/servicesPage/ServicesItem";
+import ServicesItem from './components/servicesPage/ServicesItem';
+import {
+    CSSTransition,
+    TransitionGroup,
+} from 'react-transition-group';
 
 function App() {
-  return (
-    <div className="App">
-        <Router>
-            <Switch>
-                <Route exact path={'/'}>
-                    <Banner />
-                    <Mission />
-                    <WeAre />
-                    <OurServices />
-                    <Performance />
-                    <Contact />
-                </Route>
-                {
-                    SERVICES_DATA.map(service =>
-                        <Route path={service.path} key={service.title}>
-                            <div className={'our-services-page'}>
-                                <OurServices />
-                                <ServicesItem
-                                    title={service.title}
-                                    paragraph1={service.paragraph1}
-                                    paragraph2={service.paragraph2}
-                                    paragraph3={service.paragraph3}
-                                    paragraph4={service.paragraph4}
-                                    paragraph5={service.paragraph5}
-                                    paragraph6={service.paragraph6}
-                                    image={service.image}
-                                />
-                            </div>
-                        </Route>
+    return (
+        <div className="App">
+            <Router>
+                <Route render={({ location }) => {
+
+                    return (
+                        <TransitionGroup>
+                            <CSSTransition
+                                key={location.key}
+                                timeout={450}
+                                classNames="fade"
+                            >
+                                <Switch location={location}>
+                                    <Route exact path={'/'}>
+                                        <Banner />
+                                        <Mission />
+                                        <WeAre />
+                                        <OurServices />
+                                        <Performance />
+                                        <Contact />
+                                    </Route>
+                                    {
+                                        SERVICES_DATA.map(service =>
+                                            <Route path={service.path} key={service.title}>
+                                                <div className={'our-services-page'}>
+                                                    <div className={'transition-overlay'} />
+                                                    <OurServices />
+                                                    <ServicesItem
+                                                        title={service.title}
+                                                        paragraph1={service.paragraph1}
+                                                        paragraph2={service.paragraph2}
+                                                        paragraph3={service.paragraph3}
+                                                        paragraph4={service.paragraph4}
+                                                        paragraph5={service.paragraph5}
+                                                        paragraph6={service.paragraph6}
+                                                        image={service.image}
+                                                    />
+                                                </div>
+                                            </Route>
+                                        )
+                                    }
+                                </Switch>
+                            </CSSTransition>
+                        </TransitionGroup>
                     )
-                }
-            </Switch>
-        </Router>
-    </div>
+                }} />
+            </Router>
+        </div>
   );
 }
 
