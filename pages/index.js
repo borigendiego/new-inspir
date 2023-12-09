058327c2b32cad24424ce45c41a7685080a88ba7
Diego Borigen <diego@iddigital.com.au>
2023-12-09 08:39:32 -0300

    Under constrution screen

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,22 @@ import Performance from '../components/performance';
 function App() {
     return (
         <div className='App'>
-            <Banner />
-            <Mission />
-            <WeAre />
-            <OurServices />
-            <Performance />
-            <Contact />
+            <div className={'under-construction'}>
+                <div className={'content-wrapper'}>
+                    <div className={'text-wrapper'}>
+                        <img src={'/assets/under_c_logo.png'} alt={'Under construction'} style={{marginBottom: 30}} />
+                        <h5 style={{marginBottom: 10}}>Website is</h5>
+                        <h4 style={{marginBottom: 10}}>UNDER CONSTRUCTION</h4>
+                        <p style={{marginBottom: 20}}>We are renewing ourselves. We will be back soon!</p>
+                        <h6>
+                            <a href={'mailto:bregnier@inspirperformance.com'}>
+                                bregnier@inspirperformance.com
+                            </a>
+                        </h6>
+                    </div>
+                    <img className={'illustration'} src={'/assets/under_c_illustration.png'} alt={'Under construction'} />
+                </div>
+            </div>
         </div>
     );
 }
